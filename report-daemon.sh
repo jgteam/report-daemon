4914c2fb157ad3c11d42cd5fbe20e65bb3e05ceb
Jannis Günsche <jannis.guensche@stud.h-da.de>
2023-11-26 22:45:34 +0100

    first commit

---
+++ report-daemon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+curl -H "Content-type: multipart/form-data" \
+     -F server_access_token="$(echo $REPORT_SERVER_TOKEN)" \
+     -F server_hostname="$(hostname)" \
+     -F server_frontend_url="$(echo $SERVER_FRONTEND_URL)" \
+     -F server_ipv4="$()" \
+     -F server_ipv6="$(curl ifconfig.me)" \
+     -F server_os="$(cat /etc/*-release | egrep "PRETTY_NAME|VERSION_ID" | cut -d = -f 2 | tr -d '"' | tac | tr '\n' ' ')" \
+     -F server_mysql_version="$(mysql -V)" \
+     -F server_mysql_user="$(echo $SERVER_MYSQL_USER)" \
+     -F server_mysql_database="$(echo $SERVER_MYSQL_DATABASE)" \
+     -F server_php_version="$(php -v)" \
+     -F server_cpu_usage_perc="$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')" \
+     -F server_ram_usage_perc="$(free | grep Mem | awk '{print $3/$2 * 100.0}')" \
+     -F server_storage_usage_perc="$(df --output=pcent / | grep -v Use)" \
+     -F server_cpu_cores="$(nproc --all)" \
+     -F server_ram_quota_mb="$(cat /proc/meminfo | grep MemTotal | awk '{print ($2/1000)}')" \
+     -F server_storage_quota_mb="$(df --output=size -h -BM / | grep -v 1M-blocks)" \
+     -F server_status_code="$()" \
+     -F server_status_note="$(echo $SERVER_STATUS_NOTE)" \
+     -F server_internal_note="$()" \
+     -F server_daemon_version="V 0.1" \
+     -F server_last_deamon_update="$()" \
+     -F server_last_deamon_update_check="$()" \
+     -F server_last_backup="$()" \
+     -F server_backup_url="$()" \
+     -F server_backup_htaccess_user="$()" \
+     -F server_backup_htaccess_password="$()" \
+     -X POST \
+     $(echo $REPORT_SERVER_URL)
